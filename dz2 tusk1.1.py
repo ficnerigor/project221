b2a9c1b2cb7f4100ba512a3f8226859f0d9e9b5e
IgorKlub <ficnerigor.com@gmail.com>
2020-05-15 16:42:30 +0300

    2tusk change

---
+++ dz2 tusk1.1.py
@@ -0,0 +1,27 @@
+import calendar
+
+d=int(input('day:'))
+m=int(input('month:'))
+y=int(input('year:'))
+day=calendar.weekday(y,m,d)
+if day==0:
+    print('Monday')
+elif day==1:
+    print('Tuesday')
+elif day==2:
+    print('Wednesday')
+elif day==3:
+    print('Thursday')
+elif day==4:
+    print('Friday')
+elif day==5:
+    print('Saturday')
+elif day==6:
+    print('Saturday')
+
+
+
+#Пользователь вводит дату своего рождения в формате DD.MM.YYYY. Вывести
+#название дня недели, в который родился пользователь.
+
+
